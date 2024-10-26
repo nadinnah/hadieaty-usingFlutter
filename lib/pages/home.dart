6b1db420d41417657fd1d87a05339e7f7c00c90a
nadinnah <nadinahmedmohamed@gmail.com>
2024-10-26 22:19:31 +0300

    add bottomNavBar, add ListView.builder to dynamically add friends

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:hadieaty/navigationMenu.dart';
+
+
+List friends=["Nadin", "W", "haji", "l", "y"];
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -8,65 +12,93 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  String searchGiftList = TextEditingController().text;
+  //String searchGiftList = TextEditingController().text;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor:const Color(0xffefefef),
       appBar: AppBar(
         title: const Center(
             child: Text(
               'Hadieaty',
-              style: TextStyle(fontSize: 30, fontWeight: FontWeight.w600),
+              style: TextStyle(fontSize: 30, fontWeight: FontWeight.w600, color: Colors.black),
             )),
-        toolbarHeight: 50,
-        backgroundColor: const Color(0xffe7e0dd),
+      toolbarHeight: 60,
+        backgroundColor:const Color(0xff4e615a),
       ),
-      body: SingleChildScrollView(
-        scrollDirection: Axis.vertical,
-        child: Column(
-          children: [
-            Padding(
-              padding: const EdgeInsets.all(20.0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  IconButton(
-                      onPressed: () {},
-                      icon: const Icon(
-                        Icons.person_add,
-                        size: 30,
-                        color: Color(0xFF260C1A),
-                      )),
-                  OutlinedButton(
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                IconButton(
+                    onPressed: () {},
+                    icon: const Icon(
+                      Icons.person_add,
+                      size: 30,
+                      color: Color(0xFF000000),
+                    )),
+                SizedBox(
+                  width: 250,
+                  child: OutlinedButton(
                       onPressed: () {},
                       style:
                           OutlinedButton.styleFrom(
-                            side: const BorderSide(width: 3.0, color: Color(0xFF260C1A)),
+                            backgroundColor: Color(0xff738881),
+                            side: const BorderSide(width: 2.0, color: Color(
+                                0xFF000000)),
+                            padding: EdgeInsets.symmetric(horizontal: 5),
                             ),
                       child: const Row(
                         children: [
-
                           Icon(
                             Icons.add,
                             size: 30,
-                            color: Color(0xFF260C1A),
+                            color: Color(0xFF0B0A0A),
                           ),
-                          SizedBox(width: 10),
+                          SizedBox(width: 8),
                           Text(
                             'Create new event/list',
-                            style: TextStyle(fontSize: 20, color: Color(0xFF260C1A)),
+                            style: TextStyle(fontSize: 20, color: Color(
+                                0xFF000000)),
                           )
                         ],
-                      ))
-                ],
-              ),
+                      )),
+                )
+              ],
             ),
+          ),
+        // Row(children: [
+        //
+        // ],), search implementation later, will make a class for it
 
-          ],
+        Expanded(child: ListView.builder(itemCount: friends.length, itemBuilder: (context,index){
+          return Card(
+            color:  Color(0xfffbfafa),
+            margin: EdgeInsets.fromLTRB(30, 10, 30, 10), //30 margin left and right, keeping it consistent
+            child: ListTile(
+              contentPadding: EdgeInsets.all(15.0),
+              onTap: (){},
 
-        ),
+              leading: CircleAvatar(radius: 30,backgroundColor: Colors.black,),
+              title: Text(friends[index]),
+              subtitle: Text('phone number'),
+              trailing: Text('upcoming events:'),
+
+
+
+
+            ),
+          );
+        },),),
+
+        ],
+
       ),
+    bottomNavigationBar:  NavigationMenu(),
 
     );
   }
