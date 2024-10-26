4b3a34b463e846d686326c211a273e6b82d6aa73
nadinnah <nadinahmedmohamed@gmail.com>
2024-10-26 22:19:31 +0300

    add bottomNavBar, add ListView.builder to dynamically add friends

--- lib/navigationMenu.dart
+++ lib/navigationMenu.dart
@@ -1,13 +1,32 @@
 import 'package:flutter/material.dart';
 
-class NavigationMenu extends StatelessWidget {
+class NavigationMenu extends StatefulWidget {
   const NavigationMenu({super.key});
 
   @override
+  State<NavigationMenu> createState() => _NavigationMenuState();
+}
+
+class _NavigationMenuState extends State<NavigationMenu> {
+  int _currentIndex=0;
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      bottomNavigationBar: NavigationBar(destinations:
-        [Container(), Container(),]),
+    return BottomNavigationBar(
+      selectedItemColor: Color(0xFF99B5AA),
+      backgroundColor:Color(0xFF6A817A) ,
+      currentIndex: _currentIndex,
+      onTap: (int newIndex) {
+        setState(() {
+          _currentIndex = newIndex;
+        });
+      },
+      items: const [
+        BottomNavigationBarItem(label: 'Home', icon: Icon(Icons.home, color: Color(
+            0xFF000000),)),
+        BottomNavigationBarItem(label: 'Profile', icon: Icon(Icons.person,color: Color(
+            0xFF000000),)),
+      ],
     );
   }
 }
