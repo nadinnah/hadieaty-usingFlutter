820e8f62ccf21b67ebcd4c384b829810c42e1d71
nadinnah <nadinahmedmohamed@gmail.com>
2024-10-29 09:47:05 +0300

    updating pages

--- lib/pages/giftList.dart
+++ lib/pages/giftList.dart
@@ -29,7 +29,7 @@ class _GiftListState extends State<GiftList> {
           Spacer(),
           Container(
             child: Image.asset(
-              'lib/assets/giftBox.png', // Replace with your image URL.
+              'lib/assets/giftBox.png',
               height: 200,
               width: double.infinity,
               fit: BoxFit.cover,
