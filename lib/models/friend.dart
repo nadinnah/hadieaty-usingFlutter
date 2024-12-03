fb5f4c22433716b7ea95a438c9e99b53780a2ac3
nadinnah <nadinahmedmohamed@gmail.com>
2024-12-03 21:10:55 +0200

    integrating my other projects files into this project

---
+++ lib/models/friend.dart
@@ -0,0 +1,13 @@
+class Friend {
+  String name;
+  String profilePicture;
+  String phone;
+  int upcomingEventsCount; // New field for upcoming events
+
+  Friend({
+    required this.name,
+    required this.profilePicture,
+    required this.phone,
+    required this.upcomingEventsCount,
+  });
+}
---
+++ lib/views/auth/login_page.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:hadieaty/controllers/auth_controller.dart';
+import 'signup_page.dart';
+import 'package:hadieaty/views/home_page.dart';
+
+class LoginPage extends StatefulWidget {
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
+  Authenticate Auth = Authenticate();
+
+  String errorMessage = '';
+
+  // Login method
+  Future<void> login() async {
+    if (emailController.text.isEmpty || passwordController.text.isEmpty) {
+      setState(() {
+        errorMessage = 'Please enter both email and password';
+      });
+      return;
+    }
+
+      bool status = await Auth.Sign_in(emailController.text, passwordController.text);
+      // On success, navigate to the home page (or next page)
+      if (status) {
+        Navigator.pushReplacementNamed(context, '/home');
+      }else{
+      setState(() {
+       errorMessage = 'Login failed: Invalid credentials';
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Login')),
+      body: Padding(
+        padding: EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            TextField(
+              controller: emailController,
+              decoration: InputDecoration(labelText: 'Email'),
+            ),
+            TextField(
+              controller: passwordController,
+              obscureText: true,
+              decoration: InputDecoration(labelText: 'Password'),
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: login,
+              child: Text('Login'),
+            ),
+            TextButton(
+              onPressed: () {
+                Navigator.pushNamed(context, '/signup');
+              },
+              child: Text("Don't have an account? Sign up"),
+            ),
+            if (errorMessage.isNotEmpty)
+              Text(
+                errorMessage,
+                style: TextStyle(color: Colors.red),
+              ),
+          ],
+        ),
+      ),
+    );
+  }
+}
